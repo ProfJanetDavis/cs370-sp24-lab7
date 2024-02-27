acec8fcacb9088c5c5aa626d09ea8455a3fb3bab
Janet Davis <davisj@whitman.edu>
2024-02-27 13:32:08 -0800

    Exercise 2: Timing Blocks

---
+++ SDX-ch8/_timer.py
@@ -0,0 +1,27 @@
+from mock_object import Fake
+from time import time
+
+class Timer():
+    def __init__(self):
+        self.start = None
+
+    def __enter__(self):
+        self.start = time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print(f"Elapsed time: {self.elapsed()} seconds")
+
+    def elapsed(self):
+        return time() - self.start
+        
+with Timer() as timer:
+    print(timer.elapsed())
+    for i in range(1000000):
+        pass
+    print(timer.elapsed())
+    for i in range(10000000):
+        pass
+    print(timer.elapsed())
+    for i in range(100000000):
+        pass
