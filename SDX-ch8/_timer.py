046472c7d01b67e5a9ca504aeedac5bb0cdcfb8e
Janet Davis <davisj@whitman.edu>
2024-02-27 13:39:25 -0800

    Make _timer a library

--- SDX-ch8/_timer.py
+++ SDX-ch8/_timer.py
@@ -15,13 +15,14 @@ class Timer():
     def elapsed(self):
         return time() - self.start
         
-with Timer() as timer:
-    print(timer.elapsed())
-    for i in range(1000000):
-        pass
-    print(timer.elapsed())
-    for i in range(10000000):
-        pass
-    print(timer.elapsed())
-    for i in range(100000000):
-        pass
+if __name__ == '__main__':
+    with Timer() as timer:
+        print(timer.elapsed())
+        for i in range(1000000):
+            pass
+        print(timer.elapsed())
+        for i in range(10000000):
+            pass
+        print(timer.elapsed())
+        for i in range(100000000):
+            pass
